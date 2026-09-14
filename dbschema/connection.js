c26498571840e8743f8ddd42a67fad8892a2e66d
Devyansh <devyanshawasthi0716@gmail.com>
2026-09-14 16:04:25 +0530

    removed old js files

--- dbschema/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  await mongoose.connect(process.env.mongoURL);
-  console.log('MongoDB connected!');
-};
-
-module.exports = { connectDB: connectDB };
